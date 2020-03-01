50270caf2829e81082035d6b82cd42afae6d1541
richardstvn <richardstvn@gmail.com>
2020-03-01 18:20:53 +1030

    Finished React

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Header from "./common/Header";
+import HomePage from "./home/HomePage";
+import AboutPage from "./about/AboutPage";
+import PageNotFound from "./PageNotFound";
+import CoursesPage from "./course/CoursePage";
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return <h1>Sorry, this URI is not existing.</h1>;
+};
+
+export default PageNotFound;
