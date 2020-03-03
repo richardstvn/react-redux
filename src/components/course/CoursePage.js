818a83ebd9aaa9fbaa785536544584ec7b237e18
Richard.Chu <Richard.Chu@unisa.edu.au>
2020-03-03 17:05:25 +1030

    redux changes

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,8 +1,47 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  /**
+   * Class fields
+   */
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  /**
+   * Events
+   */
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
+  /**
+   * UI
+   */
   render() {
-    return <h1>Courses</h1>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <label>
+          Title:
+          <input
+            type="text"
+            value={this.state.course.title}
+            onChange={this.handleChange}
+          ></input>
+          <input type="submit" value="Save"></input>
+        </label>
+      </form>
+    );
   }
 }
 
